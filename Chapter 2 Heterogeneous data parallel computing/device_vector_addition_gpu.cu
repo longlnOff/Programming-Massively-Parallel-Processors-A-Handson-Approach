4982d2d313d3d4cfff74540ceaec2ec68d1887a2
longlnoff <longlnofficial@gmail.com>
2024-05-10 07:29:43 +0700

    complete chapter 2

---
+++ Chapter 2 Heterogeneous data parallel computing/device_vector_addition_gpu.cu
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <iomanip>
+
+__global__
+void vecAddKernel(float* A, float* B, float* C, size_t N)
+{
+    size_t i = threadIdx.x + blockDim.x * blockIdx.x;
+    if (i < N)
+        C[i] = A[i] + B[i];
+    
+}
+
+void vecAdd(float* A, float* B, float* C, int N) {
+    size_t size = N * sizeof(float);
+    float* A_d, * B_d, * C_d;
+
+    // Part 1: Allocate device memory for A, B and C
+    // Copy A and B to device memory
+    cudaMalloc((void**)(&A_d), size);
+    cudaMalloc((void**)(&B_d), size);
+    cudaMalloc((void**)(&C_d), size);
+    cudaMemcpy(A_d, A, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(B_d, B, size, cudaMemcpyHostToDevice);
+
+
+    // Part 2: Call kernel - to launch a grid of threads
+    // to perform the actual vector addition
+    // vecAddKernel<<<ceil(N/256.0), 256>>>(A_d, B_d, C_d, N);
+
+
+    // Part 3: Copy C from the device memory 
+    cudaMemcpy(C, C_d, size, cudaMemcpyDeviceToHost);
+    // Free device vectors
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(C_d);
+}
+
+int main(void)
+{
+    size_t N = 1000;
+    std:: cout << "Number of elements in array: ";
+    std::cout << N
+              << std::setw(8)
+              << std::endl;
+    size_t size = N * sizeof(float);
+
+    float* A_h = (float*)malloc(size);
+    float* B_h = (float*)malloc(size);
+    float* C_h = (float*)malloc(size);
+
+    for (int i = 0; i < N; i++) {
+        A_h[i] = 1.0f;
+        B_h[i] = 2.0f;
+    }
+
+
+}
---
+++ Chapter 2 Heterogeneous data parallel computing/host_vector_addition_cpu.cu
@@ -0,0 +1,31 @@
+#include <iostream>
+
+
+void vecAdd(float* A_h, float* B_h, float* C_h, int N) {
+    for (int i = 0; i < N; i++) {
+        C_h[i] = A_h[i] + B_h[i];
+    }
+}
+
+int main(void)
+{
+    int N = 1 << 20;
+    size_t size = N * sizeof(float);
+
+    float* A_h = (float*)malloc(size);
+    float* B_h = (float*)malloc(size);
+    float* C_h = (float*)malloc(size);
+
+    for (int i = 0; i < N; i++) {
+        A_h[i] = 1.0f;
+        B_h[i] = 2.0f;
+    }
+
+    vecAdd(A_h, B_h, C_h, N);
+
+    free(A_h);
+    free(B_h);
+    free(C_h);
+
+    return 0;
+}
