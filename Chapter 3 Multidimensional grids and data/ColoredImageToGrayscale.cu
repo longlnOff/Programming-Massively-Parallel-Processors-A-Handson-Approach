a2523debe12343b2382aec7156ac0e047405ac65
longln <longlnofficial@gmail.com>
2024-05-13 10:14:09 +0700

    update chapter 3

--- Chapter 3 Multidimensional grids and data/ColoredImageToGrayscale.cu
+++ Chapter 3 Multidimensional grids and data/ColoredImageToGrayscale.cu
@@ -1,8 +1,5 @@
 #include <iostream>
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "header_stb.h"
-#include "stb_image_write.h"
+#include <assert.h>
 
 // Convert colored image to grayscale kernel
 __global__
@@ -12,7 +9,7 @@ void Colored2GrayscaleKernel(unsigned char* Pin, unsigned char* Pout, int width,
     size_t column_index {blockDim.x * blockIdx.x + threadIdx.x};
 
     // check whether thread grid fit with images
-    if (row_index < height && column_index < width)
+    if (column_index < width && row_index < height)
     {
         // Get grayscale offset
         size_t GrayOffset {row_index * width + column_index};
@@ -21,9 +18,9 @@ void Colored2GrayscaleKernel(unsigned char* Pin, unsigned char* Pout, int width,
         size_t ColoredOffset {GrayOffset * 3};
 
         // Get RGB values
-        unsigned char RedValue = Pin[ColoredOffset];
-        unsigned char GreenValue = Pin[ColoredOffset + 1];
-        unsigned char BlueValue = Pin[ColoredOffset + 2];
+        unsigned char RedValue      = Pin[ColoredOffset + 0];
+        unsigned char GreenValue    = Pin[ColoredOffset + 1];
+        unsigned char BlueValue     = Pin[ColoredOffset + 2];
 
         // Get gray value from RGB
         Pout[GrayOffset] = 0.21f*RedValue + 0.71f*GreenValue + 0.07f*BlueValue;
@@ -33,16 +30,18 @@ void Colored2GrayscaleKernel(unsigned char* Pin, unsigned char* Pout, int width,
 
 void Colored2Grayscale(unsigned char* Pin_h, unsigned char* Pout_h, int width, int height)
 {
-    size_t SizeAlloc {static_cast<size_t>(width * height * 3 * sizeof(unsigned char))};
+    size_t SizeAllocColored {static_cast<size_t>(3 * width * height * sizeof(unsigned char))};
+    size_t SizeAllocGray {static_cast<size_t>(width * height * sizeof(unsigned char))};
+    
     unsigned char* Pin_d {};
     unsigned char* Pout_d {};
 
     // Allocate device's memory for Pin and Pout
-    cudaMalloc((void **) &Pin_d, SizeAlloc);
-    cudaMalloc((void **) &Pout_d, SizeAlloc / 3);
+    assert(cudaMalloc((void **) &Pin_d, SizeAllocColored) == cudaSuccess);
+    assert(cudaMalloc((void **) &Pout_d, SizeAllocGray) == cudaSuccess);
 
     // Copy Pin to device memory
-    cudaMemcpy(Pin_d, Pin_h, SizeAlloc, cudaMemcpyHostToDevice);
+    assert(cudaMemcpy(Pin_d, Pin_h, SizeAllocColored, cudaMemcpyHostToDevice) == cudaSuccess);
 
     // Call kernel function
     dim3 Blockdim {static_cast<unsigned int>(16), 
@@ -58,8 +57,7 @@ void Colored2Grayscale(unsigned char* Pin_h, unsigned char* Pout_h, int width, i
     std::cout << "Here" << std::endl;
 
     // Copy Pout from device to host
-    cudaMemcpy(Pout_h, Pout_d, SizeAlloc / 3, cudaMemcpyDeviceToHost);
-    std::cout << Pout_h[0] << std::endl;
+    assert(cudaMemcpy(Pout_h, Pout_d, SizeAllocGray, cudaMemcpyDeviceToHost) == cudaSuccess);
 
     // Free device's memory
     cudaFree(Pin_d);
@@ -70,26 +68,19 @@ void Colored2Grayscale(unsigned char* Pin_h, unsigned char* Pout_h, int width, i
 
 int main()
 {
-    int width {}, height {}, componentCount {};
-    unsigned char* ImageData {stbi_load("1.png", &width, &height, &componentCount, 3)};
-    std::cout << sizeof(unsigned char) << std::endl;
-    std::cout << "Channel in file: " << componentCount << std::endl;
+    // int width {}, height {}, componentCount {};
+    // unsigned char* ImageData {stbi_load("1.png", &width, &height, &componentCount, 3)};
+    // std::cout << "Channel in file: " << componentCount << std::endl;
 
-    if (!ImageData)
-    {
-        std::cout << "Open PNG file." << std::endl;
-    }
+    // // Validate image size
+    // std::cout << width << " X " << height << std::endl;
 
+    // unsigned char* GrayImage {};
 
-    // Validate image size
-    std::cout << width << " X " << height << std::endl;
-
-    unsigned char* GrayImage {};
-
-    Colored2Grayscale(ImageData, GrayImage, width, height);
-    // Write image back to disk
-    stbi_write_png("image_gray.png", width, height, 3, GrayImage, 3 * width);
-
+    // Colored2Grayscale(ImageData, GrayImage, width, height);
+    // std::cout << "Hello" << std::endl;
+    // // Write image back to disk
+    // // stbi_write_png("image_gray.png", width, height, 3, GrayImage, 3 * width);
 
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Programming-Massively-Parallel-Processors-A-Handson-Approach
+
+- Code reference: [Code reference](https://github.com/nvixnu/pmpp__programming_massively_parallel_processors/tree/master)
+- Install opencv c++: [Install opencv c++](https://docs.opencv.org/4.x/d7/d9f/tutorial_linux_install.html)
