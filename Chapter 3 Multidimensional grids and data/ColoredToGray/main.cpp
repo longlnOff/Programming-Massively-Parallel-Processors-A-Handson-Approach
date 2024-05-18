ed216d112677cc5fc671247726e257db3940d75e
longlnoff <longlnofficial@gmail.com>
2024-05-18 15:30:05 +0700

    change code main in Colored to gray chapter 3

--- Chapter 3 Multidimensional grids and data/ColoredToGray/main.cpp
+++ Chapter 3 Multidimensional grids and data/ColoredToGray/main.cpp
@@ -15,21 +15,21 @@ int main()
         }
  
     u_char* input {new u_char[image.rows * image.cols * 3]};
-    u_char* output_d {new u_char[image.rows * image.cols]};
-    u_char* output_h {new u_char[image.rows * image.cols]};
+    u_char* GPU_output {new u_char[image.rows * image.cols]};
+    u_char* CPU_output {new u_char[image.rows * image.cols]};
 
     
     memcpy(input, image.data, image.rows * image.cols * 3);
 
-    Colored2Grayscale(input, output_d, image.rows, image.cols);
-    ch3__color_to_grayscale_host(input, output_h, image.rows, image.cols);
+    Colored2Grayscale(input, GPU_output, image.rows, image.cols);
+    ch3__color_to_grayscale_host(input, CPU_output, image.rows, image.cols);
 
-    Mat grayImage_device {image.rows, image.cols, CV_8UC1, output_d};
-    Mat grayImage_host {image.rows, image.cols, CV_8UC1, output_h};
+    Mat grayImage_GPU {image.rows, image.cols, CV_8UC1, GPU_output};
+    Mat grayImage_CPU {image.rows, image.cols, CV_8UC1, CPU_output};
 
 
-    imwrite("../Images/gray_ship_device.png", grayImage_device);
-    imwrite("../Images/gray_ship_host.png", grayImage_host);
+    imwrite("../Images/gray_ship_GPU.png", grayImage_GPU);
+    imwrite("../Images/gray_ship_CPU.png", grayImage_CPU);
 
 
     return 0;
